da74e4e07d3a081e7525105231005381ec16b554
Holland Blumer <63265395+hollandblumer6@users.noreply.github.com>
2024-01-24 12:35:23 -0500

    fixing milestones

--- app/src/components/brand/milestone/MilestoneTracker.jsx
+++ app/src/components/brand/milestone/MilestoneTracker.jsx
@@ -3,7 +3,11 @@ import { DataStore, SortDirection } from "@aws-amplify/datastore";
 import { Milestone, Suggestion } from "../../../models";
 import "../../../styles/brand/milestone/milestone.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faThumbsUp, faEllipsis } from "@fortawesome/free-solid-svg-icons";
+import {
+  faThumbsUp,
+  faEllipsis,
+  faHeartCirclePlus,
+} from "@fortawesome/free-solid-svg-icons";
 
 function formatDate(date) {
   const parsedDate = new Date(date);
@@ -16,6 +20,7 @@ function formatDate(date) {
 function MilestoneTracker({ businessname, suggestionID }) {
   const [loadedSuggestion, setLoadedSuggestion] = useState(null);
   const [groupedMilestones, setGroupedMilestones] = useState([]);
+  const [expandedGroups, setExpandedGroups] = useState({});
 
   useEffect(() => {
     const getData = async () => {
@@ -48,8 +53,18 @@ function MilestoneTracker({ businessname, suggestionID }) {
           return acc;
         }, {});
 
-        // Set the grouped milestones in the state
+        // Initialize expanded state for each group
+        const initialExpandedState = Object.keys(groupedBySuggestion).reduce(
+          (acc, currentSuggestionID) => {
+            acc[currentSuggestionID] = false;
+            return acc;
+          },
+          {}
+        );
+
+        // Set the grouped milestones and expanded state in the state
         setGroupedMilestones(groupedBySuggestion);
+        setExpandedGroups(initialExpandedState);
       } catch (err) {
         console.error(err);
       }
@@ -57,6 +72,13 @@ function MilestoneTracker({ businessname, suggestionID }) {
 
     getData();
   }, [businessname, suggestionID]);
+
+  const toggleGroup = (suggestionID) => {
+    setExpandedGroups((prevExpanded) => ({
+      ...prevExpanded,
+      [suggestionID]: !prevExpanded[suggestionID],
+    }));
+  };
 
   return (
     <div className="milestone">
@@ -67,39 +89,94 @@ function MilestoneTracker({ businessname, suggestionID }) {
               <div className="milestone-title">
                 {loadedSuggestion?.suggestion}
               </div>
+
+              {/* Render the first and last milestones always */}
               <div className="milestone-update">
-                {groupedMilestones[currentSuggestionID].map(
-                  (milestone, milestoneIndex, milestonesArray) => (
-                    <div key={milestoneIndex}>
-                      <div className="actual-milestone">
-                        <div
-                          className={`milestone-dot ${
-                            milestoneIndex === milestonesArray.length - 1
-                              ? "last-milestone-dot"
-                              : ""
-                          }`}
-                        ></div>{" "}
-                        <div className="actual-milestone-content">
-                          <div>{milestone.milestone}</div>
-                          <div className="text">
-                            {" "}
-                            {formatDate(milestone.updatedAt)}
+                <div className="actual-milestone-1">
+                  <div className={`milestone-dot first-milestone-dot`}></div>{" "}
+                  <div className="actual-milestone-content">
+                    <div>
+                      {groupedMilestones[currentSuggestionID][0].milestone}
+                    </div>
+                    <div className="text">
+                      {formatDate(
+                        groupedMilestones[currentSuggestionID][0].updatedAt
+                      )}
+                    </div>
+                  </div>
+                  <FontAwesomeIcon icon={faHeartCirclePlus} color="#939392" />
+                  <FontAwesomeIcon icon={faEllipsis} />
+                </div>
+                {groupedMilestones[currentSuggestionID].length > 2 && (
+                  <button
+                    onClick={() => toggleGroup(currentSuggestionID)}
+                    className="toggle-button"
+                  >
+                    {expandedGroups[currentSuggestionID]
+                      ? "Collapse"
+                      : "Expand"}
+                  </button>
+                )}
+                {expandedGroups[currentSuggestionID] &&
+                  groupedMilestones[currentSuggestionID].map(
+                    (milestone, milestoneIndex, milestonesArray) =>
+                      // Start the loop from index 1 and stop before the last index
+                      milestoneIndex > 0 &&
+                      milestoneIndex < milestonesArray.length - 1 && (
+                        <div key={milestoneIndex}>
+                          <div className="actual-milestone">
+                            <div
+                              className={`milestone-dot ${
+                                milestoneIndex === milestonesArray.length - 1
+                                  ? "last-milestone-dot"
+                                  : ""
+                              }`}
+                            ></div>{" "}
+                            <div className="actual-milestone-content">
+                              <div>{milestone.milestone}</div>
+                              <div className="text">
+                                {formatDate(milestone.updatedAt)}
+                              </div>
+                            </div>
+                            <FontAwesomeIcon
+                              icon={faHeartCirclePlus}
+                              color="#939392"
+                            />
+                            <FontAwesomeIcon icon={faEllipsis} />
                           </div>
                         </div>
-                        <FontAwesomeIcon icon={faThumbsUp} />
-                        <FontAwesomeIcon icon={faEllipsis} />
-                      </div>
+                      )
+                  )}
+
+                <div className="actual-milestone">
+                  <div className={`milestone-dot last-milestone-dot`}></div>{" "}
+                  <div className="actual-milestone-content">
+                    <div>
+                      {
+                        groupedMilestones[currentSuggestionID][
+                          groupedMilestones[currentSuggestionID].length - 1
+                        ].milestone
+                      }
                     </div>
-                  )
-                )}
-                <div className="project-incompleted">
-                  {" "}
-                  <div className="incomplete-dot"> </div>
-                  <p>finished project</p>
+                    <div className="text">
+                      {formatDate(
+                        groupedMilestones[currentSuggestionID][
+                          groupedMilestones[currentSuggestionID].length - 1
+                        ].updatedAt
+                      )}
+                    </div>
+                  </div>
+                  <FontAwesomeIcon icon={faHeartCirclePlus} color="#939392" />
+                  <FontAwesomeIcon icon={faEllipsis} />
                 </div>
               </div>
             </div>
           ))}
+          <div className="project-incompleted">
+            {" "}
+            <div className="incomplete-dot"> </div>
+            <p>finished project</p>
+          </div>
         </div>
       )}
     </div>
